68bf90fdca3bfc4b210ea1d43dcdc3600f74298c
Lucas Viana <luks79180@gmail.com>
2025-10-02 15:44:48 -0300

    1.0

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace csharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("-- PROGRAMA DO AÇAÍ --");
+
+        Console.WriteLine("Informe a quantidade de açaís pequenos:");
+        string qtdAcaiPequeno = Console.ReadLine();
+
+        Console.WriteLine("Informe a quantidade de açaís médios:");
+        string qtdAcaiMedio = Console.ReadLine();
+
+        Console.WriteLine("Informe a quantidade de açaís grandes:");
+        string qtdAcaiGrande = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(qtdAcaiPequeno)) qtdAcaiPequeno = "0";
+        if (string.IsNullOrWhiteSpace(qtdAcaiMedio)) qtdAcaiMedio = "0";
+        if (string.IsNullOrWhiteSpace(qtdAcaiGrande)) qtdAcaiGrande = "0";
+
+        double TotalAcaiPequenoConv = Convert.ToDouble(qtdAcaiPequeno);
+        double TotalAcaiMedioConv = Convert.ToDouble(qtdAcaiMedio);
+        double TotalAcaiGrandeConv = Convert.ToDouble(qtdAcaiGrande);
+
+        double PrecoFinal = TotalAcaiPequenoConv * 13 + TotalAcaiMedioConv * 15 + TotalAcaiGrandeConv * 17.50;
+
+        Console.WriteLine($"Compra finalizada! Total a ser pago R${PrecoFinal}.");
+    }
+}
